7f62c7cbb11ebf152e876830b69b8b289c1fa416
Vinicius Mendes Martins <viniciusmmartins@gmail.com>
2023-11-20 01:17:17 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FiapStore.Controllers
+{
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet]
+
+        public IActionResult ObterUsuario()
+        {
+            return Ok("Usuário retornado com sucesso!");
+        }
+        [HttpPost]
+        public IActionResult CadastrarUsuario()
+        {
+            return Ok("Usuário criado com sucesso!");
+        }
+
+        [HttpPut]
+        public IActionResult AlterarUsuario()
+        {
+            return Ok("Usuário alterado com sucesso!");
+        }
+        [HttpDelete]
+        public IActionResult DeleteUsuario()
+        {
+            return Ok("Usuário deletado com suceso!");
+        }
+
+
+    }
+}
